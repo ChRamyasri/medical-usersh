e60e7f3bb069fe0dde4ea17228818737c1e1e96b
ChRamyasri <130270980+ChRamyasri@users.noreply.github.com>
2023-05-29 12:29:15 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install python3-pip
-sudo git clone https://github.com/elated-salman/Medical-Insurance.git
+sudo git clone https://github.com/ChRamyasri/medical-usersh.git
 cd Medical-Insurance
 sudo pip3 install -r requirements.txt
 python3 app.py
